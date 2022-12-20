2b7d794ecb70e21f10022829e1200d7d44a2499a
p-Lennart <112364552+p-Lennart@users.noreply.github.com>
2022-12-19 18:22:15 -0800

    Update truth_table.js

--- truth_table.js
+++ truth_table.js
@@ -1,7 +1,39 @@
+const colors = {
+    Reset: "\x1b[0m",
+    Bright: "\x1b[1m", 
+    Dim: "\x1b[2m",
+    Underscore: "\x1b[4m",
+    Blink: "\x1b[5m",
+    Reverse: "\x1b[7m",
+    Hidden: "\x1b[8m",
+  
+    FgBlack: "\x1b[30m",
+    FgRed: "\x1b[31m",
+    FgGreen: "\x1b[32m",
+    FgYellow: "\x1b[33m",
+    FgBlue: "\x1b[34m",
+    FgMagenta: "\x1b[35m",
+    FgCyan: "\x1b[36m",
+    FgWhite: "\x1b[37m",
+  
+    BgBlack: "\x1b[40m",
+    BgRed: "\x1b[41m",
+    BgGreen: "\x1b[42m",
+    BgYellow: "\x1b[43m",
+    BgBlue: "\x1b[44m",
+    BgMagenta: "\x1b[45m",
+    BgCyan: "\x1b[46m",
+    BgWhite: "\x1b[47m"
+};
+
+function renderTF(bool) {
+    return bool ? `${colors.FgGreen}T${colors.Reset}` : `${colors.FgRed}F${colors.Reset}`;
+}
+
 const negation = '~';
 
 const symbols = {
-    '&': 'conjunction',
+    '&': 'conjunction', 
     'v': 'disjunction',
     '>': 'implication',
     '=': 'equivalence',
@@ -48,31 +80,32 @@ function evalFunction(lh, op, rh) {
 
 const dictionary = {};
 
-const handleComponent = (component, eval) => {
-    if (typeof component !== 'string') return component;
-
-    let negation = component.indexOf('~');
-    
-    if (negation > 0) {
-        return "ERR";
-    } else {
-        let compStr = component.slice(negation + 1);
-        
+const handleComponent = (component, negation, eval) => {
+    if (typeof component === 'boolean') {
+        return (negation ? !component : component); 
+    } else if (typeof component === 'string') {
         if (eval) {
-            return (negation === 0 ? !dictionary[compStr] : dictionary[compStr])
+            return (negation ? !dictionary[component] : dictionary[component])
         } else {
-            dictionary[compStr] = 'fill';
-            return component; 
+            dictionary[component] = 'fill';
+            return (negation ? '~' : '') + component; 
         }
-    } 
+    } else {
+        if (component.op && negation) {
+            component.op = `~${component.op}`; 
+        }
+        return component;
+    }
 }
 
 const parse = (str, eval = false) => {
     // console.log(str, eval ? "EVAL" : "");
 
     let leftResult = false;
+    let leftNeg = false;
     let operator = false;
     let rightResult = false;
+    let rightNeg = false;
 
     let openBrackets = [];
     let closedBracketCount = 0;
@@ -102,11 +135,11 @@ const parse = (str, eval = false) => {
         }
 
         if (c === '(') {
-            openBrackets.push(i + 1);
+            openBrackets.push(i);
         } else if (c === ')') {
             closedBracketCount += 1;
             if (closedBracketCount === openBrackets.length) {
-                let sub = str.substring(openBrackets[0], i);
+                let sub = str.substring(openBrackets[0] + 1, i);
                 
                 closedBracketCount = 0;
                 openBrackets = [];
@@ -116,6 +149,12 @@ const parse = (str, eval = false) => {
         } else if (openBrackets.length == 0) {
             if (Object.keys(symbols).includes(c)) {
                 operator = c;
+            } else if (c === negation) {
+                if (operator) {
+                    rightNeg = !rightNeg;
+                } else {
+                    leftNeg = !leftNeg;
+                }
             } else {
                 setResult(c);
             }
@@ -129,12 +168,12 @@ const parse = (str, eval = false) => {
         return { error: `Parenthetical mismatch: ${openBrackets.length} open, ${closedBracketCount} close` };
     }
 
-    leftResult = handleComponent(leftResult, eval);
+    leftResult = handleComponent(leftResult, leftNeg, eval);
 
     if (operator) {
-        rightResult = handleComponent(rightResult, eval);
+        rightResult = handleComponent(rightResult, rightNeg, eval);
 
-        // console.log('Handling', leftResult, operator, rightResult)
+        // console.log('Handling', leftResult, operator, rightResult, leftNeg, rightNeg);
         
         if (eval) {
             // console.log('EVAL', leftResult, symbols[operator], rightResult);
@@ -147,11 +186,11 @@ const parse = (str, eval = false) => {
     }
 }
 
-function temp(str) {
-    console.log(parse(str, false));
+function evalSingle(argStr) {
+    console.log(parse(argStr, false));
     
     let vars = Object.keys(dictionary);
-    vars.sort((a, b) => str.indexOf(a) - str.indexOf(b)); // Sort variables by occurance
+    vars.sort((a, b) => argStr.indexOf(a) - argStr.indexOf(b)); // Sort variables by occurance
     
     let combinations = getBooleanCombinations(vars.length);
     
@@ -162,12 +201,12 @@ function temp(str) {
         
         for (let varNum = 0; varNum < vars.length; varNum++) {
             dictionary[vars[varNum]] = combinations[varNum][combNum];
-            logStr += `${vars[varNum]}: ${dictionary[vars[varNum]]? 'T' : 'F'} | `;
+            logStr += `${vars[varNum]}: ${renderTF(dictionary[vars[varNum]])} | `;
         }
 
-        let val = parse(str, true);
+        let val = parse(argStr, true);
         result.push({ ...dictionary, result: val });
-        logStr += `${str}: ${val? 'T' : 'F'}`;
+        logStr += `${argStr}: ${renderTF(val)}`;
 
         console.log(logStr);
     }
@@ -175,6 +214,125 @@ function temp(str) {
     return result;
 }
 
-test = temp('(p>(qvr))=(r&x)');
+function evalMultipleEquiv(argStrArr) {
+    for (let str of argStrArr) {
+        console.log(parse(str, false));
+    }
+
+    let vars = Object.keys(dictionary);
+    vars.sort((a, b) => a - b); // Sort variables alphabetically
+    
+    let combinations = getBooleanCombinations(vars.length);
+
+    let result = [];
+    
+    for (let combNum = 0; combNum < combinations[0].length; combNum++) {
+        let logStr = '';
+        
+        for (let varNum = 0; varNum < vars.length; varNum++) {
+            dictionary[vars[varNum]] = combinations[varNum][combNum];
+            logStr += `${vars[varNum]}: ${renderTF(dictionary[vars[varNum]])} | `;
+        }
+        logStr += '|';
+
+        let resultLine = {};
+
+        for (let str of argStrArr) {
+            let val = parse(str, true);
+
+            resultLine[str] = val;
+            logStr += `${str}: ${renderTF(val)} | `;
+        }
+
+        let equiv = Object.values(resultLine).every(e => evalFunction(e, 'equivalence', Object.values(resultLine)[0]));
+        resultLine.equiv = equiv;
+        
+        if (!equiv) {
+            logStr += colors.BgRed;
+        }
+        logStr += `EQUIV: ${renderTF(equiv)}`;
+
+        console.log(logStr);
+        result.push({ ...dictionary, ...resultLine });
+    }
+
+    if (result.every(e => e.equiv === true)) {
+        console.log(`${colors.FgGreen}Logically Equivalent${colors.Reset}`);
+        return [result, true];
+    } else if (result.every(e => e.equiv === false)) {
+        console.log(`${colors.FgRed}Logically Contradictory${colors.Reset}`);
+        return [result, false];
+    } else {
+        console.log('Neither Equivalent nor Contradictory')
+        return [result, 'neither'];
+    }
+
+}
+
+function evalArgValidity(argStrArr, csqStr) {
+    for (let str of argStrArr) {
+        console.log(parse(str, false));
+    }
+    console.log(parse(csqStr));
+
+    let vars = Object.keys(dictionary);
+    vars.sort((a, b) => a - b); // Sort variables alphabetically
+    
+    let combinations = getBooleanCombinations(vars.length);
+
+    let result = [];
+    
+    for (let combNum = 0; combNum < combinations[0].length; combNum++) {
+        let logStr = '';
+        
+        for (let varNum = 0; varNum < vars.length; varNum++) {
+            dictionary[vars[varNum]] = combinations[varNum][combNum];
+            logStr += `${vars[varNum]}: ${renderTF(dictionary[vars[varNum]])} | `;
+        }
+        logStr = logStr.slice(0, -1) + '| ';
+
+        let resultLine = {};
+
+        for (let str of argStrArr) {
+            let val = parse(str, true);
+            resultLine[str] = val;
+            logStr += `${str}: ${renderTF(val)} | `;
+        }
+        
+        let allArgsTrue = Object.values(resultLine).every(e => e === true);
+        // console.log(Object.values(resultLine), allArgsTrue);
+        
+        let csqVal = parse(csqStr, true);
+        resultLine[`∴${csqStr}`] = csqVal;
+        logStr += `∴ ${csqStr}: ${renderTF(csqVal)} || `;
+
+        let valid = !allArgsTrue || (csqVal === true);
+        resultLine.valid = valid;
+        // console.log(!allArgsTrue, csqVal === true);
+
+        if (!valid) {
+            logStr += colors.BgRed;
+        }
+        logStr += `VALID: ${renderTF(valid)}`;
+
+        console.log(logStr);
+        result.push({ ...dictionary, ...resultLine });
+    }
+
+    if (result.every(e => e.valid === true)) {
+        console.log(`${colors.FgGreen}Argument Valid${colors.Reset}`);
+        return [result, true];
+    } else {
+        console.log(`${colors.FgRed}Argument Inalid${colors.Reset}`);
+        return [result, false];
+    }
+
+}
+
+// test = temp('~(p>q)&~(p&~q)');
+// test = evalSingle('~(p>q)&~(p&~q)');
+// test = evalMultipleEquiv(['~(p>q)', '~(p&~q)']);
+test = evalArgValidity(['a', 'a>b', 'b>c', 'c>d', 'd>e', 'e>f', 'f>g', 'g>h'], 'h');
+// test = evalArgValidity(['p>q', '~p'], '~q');
 // console.log(test);
 // console.log(JSON.stringify(test));
